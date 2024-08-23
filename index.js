e92ed7705b232f40884a8e47ae68219b3f3fd876
Satyaam38 <157888730+Satyaam38@users.noreply.github.com>
2024-08-23 21:31:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+var number=document.querySelectorAll(".drum").length;
+for(i=0;i<number;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+    switch (this.innerHTML) {
+        case "w":
+            var tom1 =new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 =new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 =new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 =new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var kick =new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "k":
+            var snare =new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "l":
+            var crash =new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        default:
+            break;
+}});}
+document.addEventListener("keydown",function(e){
+    switch (e.key) {
+        case "w":
+            var tom1 =new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 =new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 =new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 =new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var kick =new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "k":
+            var snare =new Audio("./sounds/snare.mp3");
+            snare.play();
+            break;
+        case "l":
+            var crash =new Audio("./sounds/crash.mp3");
+            crash.play();
+            break;
+        default:
+            break;
+}});
